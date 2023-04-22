4b5ee44641a298a64585490e75a04157a5430fa1
The237 <69681767+The237@users.noreply.github.com>
2023-04-22 20:12:06 +0100

    Get user input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -13,5 +15,29 @@ public class Main {
         long x = 1122222228533333335L;
         System.out.println("My x value is :"+x);
 
+        // switch variables
+        String xw = "water";
+        String y = "Kook-Aid";
+        String temp = null;
+        temp = xw;
+        xw = y;
+        y = temp;
+        System.out.println("x: "+xw);
+        System.out.println("y: "+y);
+
+        // get user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name ?");
+        String name  = scanner.nextLine();
+        System.out.print("How old are you ?");
+        int age = scanner.nextInt();
+        System.out.println("What's your favorite food");
+        String food = scanner.nextLine();
+        // clear the scanner because of the first call of nextline
+        scanner.nextLine();
+
+        System.out.println("Hello "+ name);
+        System.out.println("You are "+ age + " years old");
+        System.out.println("You like  "+food);
     }
 }
