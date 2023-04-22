96c67d8b86bd5970339dc7c8ed96231a221d8e68
The237 <69681767+The237@users.noreply.github.com>
2023-04-22 20:36:49 +0100

    Handle Math library

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // first test
-        System.out.print("Hello World");
+        /*System.out.print("Hello World");
         // variables
         // String starts with a capital because it's a reference datatype
         // not primitive like in or others
@@ -24,7 +24,7 @@ public class Main {
         xw = y;
         y = temp;
         System.out.println("x: "+xw);
-        System.out.println("y: "+y);
+        System.out.println("y: "+y);*/
 
         // get user input
         /*Scanner scanner = new Scanner(System.in);
@@ -42,13 +42,30 @@ public class Main {
         System.out.println("You like  "+food);*/
 
         // GUI program
-        String name = JOptionPane.showInputDialog("Input your name");
+        /*String name = JOptionPane.showInputDialog("Input your name");
         JOptionPane.showMessageDialog(null,"Hello "+name);
 
         int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
         JOptionPane.showMessageDialog(null,"You're "+age+" years old");
 
         double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-        JOptionPane.showMessageDialog(null,"You're "+height+" cm tall");
+        JOptionPane.showMessageDialog(null,"You're "+height+" cm tall");*/
+
+        // Math methods
+        double x = 3.14;
+        double y = -10;
+        double  z = Math.round(x);
+        System.out.println(z);
+
+        double a;
+        double b;
+        double c;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter side a: ");
+        a = scanner.nextDouble();
+        System.out.println("Enter side a: ");
+        b = scanner.nextDouble();
+        c = Math.sqrt((a*a)+(b*b));
+        System.out.print("The hypothenuse is "+c);
     }
 }
