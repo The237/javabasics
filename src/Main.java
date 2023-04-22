9fe2a76ed356abe9d27015ddc2179de2c6aecf4c
The237 <69681767+The237@users.noreply.github.com>
2023-04-22 19:53:23 +0100

    Handle java datatypes

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // first test
+        System.out.print("Hello World");
+        // variables
+        // String starts with a capital because it's a reference datatype
+        // not primitive like in or others
+        String papa = "\npapa est venu à la maison";
+        System.out.println(papa);
+        // by convention, a long is always followed by a L
+        long x = 1122222228533333335L;
+        System.out.println("My x value is :"+x);
+
+    }
+}
