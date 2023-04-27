263bee80f1788344d9489476e52c401eb9deace8
The237 <69681767+The237@users.noreply.github.com>
2023-04-27 10:51:43 +0100

    Handle:Interfaces

--- src/Main.java
+++ src/Main.java
@@ -295,6 +295,17 @@ public class Main {
         System.out.println(carE1.getMake());
         System.out.println(carE1.getModel());
         System.out.println(carE1.getYear());*/
+
+        // Interfaces
+        Rabbit rabbit = new Rabbit();
+        rabbit.flee();
+
+        Hawk hawk = new Hawk();
+        hawk.hunt();
+
+        Fish fish = new Fish();
+        fish.flee();
+        fish.hunt();
     }
     //Methods in java
     static void helloWorld(String name, int age){
